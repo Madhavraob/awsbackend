25bfb4e7a4690837aa962515f69b5ba550ae22c5
Madhavraob <madhavrao039@gmail.com>
2018-07-17 07:41:09 +0530

    removing fe code

--- frontend/src/app/cap-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CapPipePipe } from './cap-pipe.pipe';
-
-describe('CapPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CapPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
